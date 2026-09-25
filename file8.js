64843684b3a00cba18eca68ecffcdc33af97b338
MuhammadEsa-dev <muhammadesa952@gmail.com>
2026-09-25 20:49:15 +0500

    8.js

---
+++ file8.js
@@ -0,0 +1,32 @@
+// //Assignment Operators
+
+// a = 5;
+// b = 2;
+
+// a /= 4;
+// console.log("a=", a);
+
+// //Comparison Operators
+// a = 5;
+// b = 2;
+// console.log("a==b", a == b);
+
+// console.log("a != b", a != b);
+
+// p = 7;
+// q = "8";
+
+// console.log("p!==q", p !== q);
+// console.log("p>=q", p >= q);
+// console.log("7<=8", p <= q);
+
+//Logical Operators
+
+a = 6;
+b = 5;
+
+let cond1 = a < b; //false
+let cond2 = a === 5; //false
+console.log("cond1 && cond2", cond1 && cond2);
+
+console.log("cond1||cond2", cond1 || cond2);
